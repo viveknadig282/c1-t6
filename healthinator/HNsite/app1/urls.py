95ab8b42634ed213234188abaaf15ff4ec068619
Andrew McCleary <theandrewmccleary@gmail.com>
2021-10-08 23:23:46 -0400

    skelly 2.0
    updated name of environment, project, and app. integrated Postgres. python version changed to 3.8.10

--- healthinator/HNsite/app1/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views #importing views file
-
-urlpatterns = [
-    #path('', views.index, name='index'),
-    path('', views.homepage, name='home'), #mapping the homepage function
-]
