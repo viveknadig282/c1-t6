d6964c07df4b84f9b3ddd0e4fd38efbd7c69a955
Shigmah <90661077+Shigmah@users.noreply.github.com>
2021-10-31 16:08:31 -0400

    Add files via upload

---
+++ intelligentEats/IEsite/IEapp/FoodAPI.py
@@ -0,0 +1,25 @@
+import requests
+import urllib
+import json
+
+class FoodAPI:
+    #following constants should be replaced by production version and moved into settings.py
+    app_id = '5d3057d6'
+    app_key = '0a61f1c8fb7cfe232b441f8faa2afd4f'
+
+    def __init__(self, app_id, app_key):
+        if app_id is not None and app_key is not None:
+            self.app_id = app_id
+            self.app_key = app_key
+
+    #this function will throw an exception in case of error. Please catch.
+    def get_ingredients(self, upc_code):
+        url = 'https://api.edamam.com/api/food-database/v2/parser?app_id=' + self.app_id + '&app_key=' + self.app_key + '&upc=' + upc_code + '&nutrition-type=cooking'
+        
+        response = requests.get(url, headers={'Accept':'application/json'})
+        response.raise_for_status()
+        json_response = response.json()
+        ingredient_str = json_response['hints'][0]['food']['foodContentsLabel']
+        ingredients = ingredient_str.split('; ')
+        
+        return ingredients
