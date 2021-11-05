325a28dbc6af9b800b47541b562620c81167266c
Shigmah <90661077+Shigmah@users.noreply.github.com>
2021-11-04 20:36:13 -0400

    Update urls.py

--- intelligentEats/IEsite/IEapp/urls.py
+++ intelligentEats/IEsite/IEapp/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views #importing views file
 
 urlpatterns = [
+    path('ingredients/<str:ingredient_list>', views.ingredient_scores, name='ingredients'), #mapping the ingredient function
+    path('upc/<str:upc_code>', views.upc, name='upc'), #mapping the upc function
     path('results/', views.resultspage, name='results'), #mapping the resultspage function
     path("", views.homepage, name="home"), #mapping the homepage function
 ]
