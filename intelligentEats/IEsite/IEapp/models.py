b1034f6042a130db0e8e7038430811abf0256ee4
Cofeecake <90661317+Cofeecake@users.noreply.github.com>
2021-11-05 22:22:19 -0400

    Updated the scripts and models .py

--- intelligentEats/IEsite/IEapp/models.py
+++ intelligentEats/IEsite/IEapp/models.py
@@ -6,25 +6,24 @@ class ScraperArticle(models.Model):
     id                    = models.IntegerField(primary_key=True)
     article_url           = models.CharField(max_length = 50000)
     article_data          = models.CharField(max_length = 50000)
-    #fix this on delete setting!!
-    ingredients_id        = models.ForeignKey('Ingredient', models.SET_NULL, blank=True, null=True)
+    ingredients_id        = models.ForeignKey('Ingredient', on_delete=models.CASCADE, null=True)
     classification_result = models.IntegerField(default=0)
  
 class Ingredient(models.Model):
     id             = models.IntegerField(primary_key=True)
     name           = models.CharField(max_length = 50000)
-    score          = models.IntegerField(default=0)
-    within_food    = models.ManyToManyField('Food', through='IngredientFood')
+    score          = models.FloatField(default = 0.0)
+    foods          = models.ManyToManyField(through='IEapp.IngredientFood', to='IEapp.Food')
     
 
 class Food(models.Model):
     id                 = models.IntegerField(primary_key=True)
     name               = models.CharField(max_length = 50000)
-    score              = models.IntegerField(default=0)
-    ingredient_content = models.ManyToManyField('Ingredient', through='IngredientFood')
+    score              = models.FloatField(default= 1.0)
+    ingredients        = models.ManyToManyField('Ingredient', through='IngredientFood')
     
 
 class IngredientFood(models.Model):
     id             = models.IntegerField(primary_key=True)
-    ingredients = models.ForeignKey('Ingredient', models.SET_NULL, blank=True, null=True)
-    foods       = models.ForeignKey('Food', models.SET_NULL, blank=True, null=True)
+    ingredients    = models.ForeignKey('Ingredient', models.SET_NULL, blank=True, null=True)
+    foods          = models.ForeignKey('Food', models.SET_NULL, blank=True, null=True)
--- intelligentEats/IEsite/scripts/read_from_csv.py
+++ intelligentEats/IEsite/scripts/read_from_csv.py
@@ -4,48 +4,62 @@ from IEapp.models import ScraperArticle, Ingredient, Food, IngredientFood
 
 #This will be called to run 
 def run(): 
-    to_read = [
-    r'C:\Users\Admin\Downloads\ingredients_sql.csv',
-    r'C:\Users\Admin\Downloads\food_sql.csv',
-    r'C:\Users\Admin\Downloads\ingredient_food_sql.csv',
-    ]
+    # insert the files you want to read into the lists under the proper key
+    to_read = {
+    'Ingredients': [r'C:\Users\Admin\Downloads\ingredients_sql.csv'],
+    'Foods': [r'C:\Users\Admin\Downloads\food_sql.csv'],
+    'IngredientFoods':[r'C:\Users\Admin\Downloads\ingredient_food_sql.csv'],
+    'ScraperArticles' : [r'C:\Users\Admin\Downloads\scraper_articles_sql_1.csv', 
+                         r'C:\Users\Admin\Downloads\scraper_articles_sql_2.csv', 
+                         r'C:\Users\Admin\Downloads\scraper_articles_sql_3.csv',
+                         r'C:\Users\Admin\Downloads\scraper_articles_sql_4.csv'
+                         ]
+    }
 
     ScraperArticle.objects.all().delete() # get rid of any extra data stored in the database 
-    Ingredient.objects.all().delete()      # ditto
+    Ingredient.objects.all().delete()      # just for now at least while im still editing the scripts and doing mass imports
     Food.objects.all().delete()             # ditto 
     IngredientFood.objects.all().delete()  # ditto
 
-    for index, file in enumerate(to_read):
-        current_file = open(file, encoding='utf-8') # define the file to open and how to read it 
-        reader = csv.reader(current_file)        # read the file using csv module
-        next(reader)                             # advance past the header
-
-        
-
-        #reader
-        for row in reader:
-            print(row)
-            if index == 0:
-                Ingredient(
-                id   = row[0], 
-                name = row[1]
-                ).save()
-                
-
-            if index == 1:
-                Food(
-                id   = row[0], 
-                name = row[1]
-                ).save()
-                
-
-            if index == 2: 
-                IngredientFood(
-                id         = row[0], 
-                ingredients = Ingredient.objects.get(pk = row[1]), 
-                foods       = Food.objects.get(pk = row[2])
-                ).save()
-            
-        
-        
-    
+    #define a function to read the files into the database
+    def filereader(files, current_table):
+        #loops through the list within each key incase there are multiple csv files for a table
+        for file in files:
+            with open(file, 'r', encoding='utf-8') as csv_file:
+                csv_reader = csv.DictReader(csv_file)
+                #loops through all the rows in the csv file
+                for row in csv_reader:
+                    print(row)
+                    #saves each row to their respective tables
+                    if current_table == Ingredient:
+                        current_table(
+                            id = row['ID'],
+                            name = row['NAME'],
+                            score = row['SCORE']
+                        ).save()
+                    elif current_table == Food:
+                        print('food time')
+                        current_table(
+                        id = row['id'],
+                        name = row['name'],
+                        
+                        ).save()
+                    elif current_table == IngredientFood:
+                        current_table(
+                        id = row['id'],
+                        ingredients = Ingredient.objects.get(pk = int(row['ingredient_id'])),
+                        foods = Food.objects.get(pk = 1)
+                        ).save()
+                    elif current_table == ScraperArticle:
+                        x = 1
+                        current_table(
+                        id = row['ID'],
+                        article_url = row['ARTICLE_LINK'],
+                        article_data = row['ARTICLE_DATA'],
+                        ingredients_id = Ingredient.objects.get(pk = row["INGREDIENT_ID"]),
+                        classification_result = row['CLASSIFICATION_RESULT']
+                        ).save()
+    filereader(to_read['Ingredients'], Ingredient)
+    filereader(to_read['Foods'], Food)
+    filereader(to_read['IngredientFoods'], IngredientFood)
+    filereader(to_read['ScraperArticles'], ScraperArticle)
